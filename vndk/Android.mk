359e0ffca92515c34116eda82a5444bbb9fedddb
Vinay Verma <vvinay@codeaurora.org>
2018-04-30 00:24:13 +0530

    msm8996-common: Disable vndk-sp creation for VNDK enabled targets.
    Change-Id: Idf17c7ddeab922c231fc3e9bb786c45dfa54684b
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -1,3 +1,19 @@
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ifeq ($(BOARD_VNDK_VERSION),)
+$(warning ************* BOARD VNDK is not enabled - compiling vndk-sp ***************************)
 LOCAL_PATH := $(call my-dir)
 
 include $(LOCAL_PATH)/vndk-sp-libs.mk
@@ -40,3 +56,4 @@ LOCAL_MODULE := vndk-sp
 LOCAL_MODULE_TAGS := optional
 LOCAL_REQUIRED_MODULES := $(addsuffix .vndk-sp-gen,$(VNDK_SP_LIBRARIES))
 include $(BUILD_PHONY_PACKAGE)
+endif
