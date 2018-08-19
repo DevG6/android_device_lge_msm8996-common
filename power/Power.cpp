6d142684a11eb07fdfa29ab00842658a80fd0bc8
Eliminater74 <eliminater74@gmail.com>
2018-08-19 01:18:37 -0400

    [power] Rename Power Hal For LGE msm8996:
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- power/Power.cpp
+++ power/Power.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.power@1.1-service.marlin"
+#define LOG_TAG "android.hardware.power@1.1-service.lge.msm8996"
 
 #include <android/log.h>
 #include <android-base/file.h>
