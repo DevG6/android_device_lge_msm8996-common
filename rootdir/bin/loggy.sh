43e7e4c76d7ea40725939bc8a3ec3b6fa0170dc6
Eliminater74 <eliminater74@gmail.com>
2018-09-26 10:11:16 -0400

    [loggy] update with adding console-ramoops as well:
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- rootdir/bin/loggy.sh
+++ rootdir/bin/loggy.sh
@@ -4,3 +4,7 @@
 _date=`date +%F_%H-%M-%S`
 logcat -b all -v time -f  /cache/logcat_${_date}.txt &
 cat /proc/kmsg > /cache/kmsg_${_date}.txt
+if [ -e /sys/fs/pstore/console-ramoops ]
+then
+cat /sys/fs/pstore/console-ramoops > /cache/console-ramoops_${_date}.txt
+fi
